283fedf6f1efd3896b78f435d76798fbb71e6fec
Raul Costa <raulbrennersc@gmail.com>
2021-03-06 15:14:57 +0000

    add inline editor with bas64 image upload

--- easyquest/src/shared/components/RichTextField.jsx
+++ easyquest/src/shared/components/RichTextField.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
 import { Grid } from '@material-ui/core';
+import InlineEditor from 'ckeditor5-build-inline-with-base64-upload';
 
-export default ({ value, setValue, className }) => (
-  <Grid className={className}>
-    <CKEditor
-      editor={ClassicEditor}
-      data={value}
-      onChange={(event, editor) => {
-        setValue(editor.getData().trim());
-      }}
-    />
-  </Grid>
-);
+export default ({ value, setValue, className }) => {
+  const [ctrl, setCtrl] = useState(true);
+  const [id, setId] = useState('');
+  useEffect(() => {
+    if (!id) {
+      const x = uuid().replace(/[0-9]/g, 'x');
+      setId(x);
+    } else if (ctrl && id) {
+      setCtrl(false);
+      InlineEditor.create(document.querySelector(`#${id}`))
+        .then((editor) => {
+          window.editor = editor;
+          console.log(editor);
+        })
+        .catch((err) => {
+          console.error(err.stack);
+        });
+    }
+  }, [ctrl, id]);
+
+  return <div id={id} style={{ width: '200px' }} />;
+};
